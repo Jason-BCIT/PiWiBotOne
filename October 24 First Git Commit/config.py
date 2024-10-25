f8fcb2c39ef323c859ff119961442e22a4fe2cb5
Jason <108756994+Jason-BCIT@users.noreply.github.com>
2024-10-24 23:58:40 -0700

    Delete October 24 First Git Commit directory

--- October 24 First Git Commit/config.py
+++
@@ -1,5 +0,0 @@
-WIFI_SSID='YourLocalWifiNetworkName'
-WIFI_PASSWORD='YourLocalWifiNetworkPassword'
-
-# This is the network you will connect to if you hold a button down at startup
-WIFI_AP_MODE=False
--- October 24 First Git Commit/config_AP.py
+++
@@ -1,6 +0,0 @@
-WIFI_SSID='ChangeMeInConfigAP'
-WIFI_PASSWORD='123456789'
-
-# Set up a wireless hotspot with WIFI_SSID/WIFI_PASSWORD above
-# This code will run if you do NOT press and hold a button when starting the PiWiBot
-WIFI_AP_MODE=True
